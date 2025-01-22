c0a457ce639a70bb7a9e3c5a264146606bdab48f
Elchonon <ekn400@gmail.com>
2025-01-22 17:00:04 -0500

    routine card

--- client/src/components/RoutineCard.jsx
+++ client/src/components/RoutineCard.jsx
@@ -12,56 +12,67 @@ const RoutineCard = ({routine}) => {
         alert('button clicked')
     }
 
+    const days = ["","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
     return (
         <Card style={{ width: '18rem' }}>
             {routine.exercise.image ? <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> : ""}
             <Card.Body>
                 <Card.Title>{routine.exercise.name ? routine.exercise.name : "Exercise_Name"}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{routine.exercise.muscle_group ? routine.exercise.muscle_group : "Exercise_Muscle_Group"}</Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">{days[routine.day_of_the_week]} - {routine.exercise.muscle_group}</Card.Subtitle>
                 <ListGroup className="list-group-flush">
                     <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
                         <div className="ms-2 me-auto">
                             Difficulty Level
                         </div>
-                        <Badge bg="primary" pill>
+                        <Badge bg="danger" pill>
                             {routine.exercise.difficulty_level ? routine.exercise.difficulty_level : "N/A"}
-                        </Badge>
-                    </ListGroup.Item>
-                    
-                    <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
-                        <div className="ms-2 me-auto">
-                            Initial Weight
-                        </div>
-                        <Badge bg="primary" pill>
-                          {routine.initial_weight}
-                        </Badge>
-                    </ListGroup.Item>
-                    <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
-                        <div className="ms-2 me-auto">
-                            Initial Reps
-                        </div>
-                        <Badge bg="primary" pill>
-                          {routine.initial_reps}
-                        </Badge>
-                    </ListGroup.Item>
-                    <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
-                        <div className="ms-2 me-auto">
-                            Initial Sets
-                        </div>
-                        <Badge bg="primary" pill>
-                          {routine.initial_sets}
                         </Badge>
                     </ListGroup.Item>
                     <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
                         <div className="ms-2 me-auto">
                             Priority
                         </div>
-                        <Badge bg="primary" pill>
+                        <Badge bg="warning" pill>
                           {routine.priority}
                         </Badge>
                     </ListGroup.Item>
-                    
-                    
+                    <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
+                        <div className="ms-2 me-auto">
+                            Weight
+                        </div>
+                        <Badge bg="success" pill>
+                          {routine.current_weight}
+                        </Badge>
+                        &nbsp;
+                        <Badge bg="primary" pill>
+                          {routine.initial_weight}
+                        </Badge>
+                    </ListGroup.Item>
+                    <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
+                        <div className="ms-2 me-auto">
+                            Reps
+                        </div>
+                        <Badge bg="success" pill>
+                          {routine.current_reps}
+                        </Badge>
+                        &nbsp;
+                        <Badge bg="primary" pill>
+                          {routine.initial_reps}
+                        </Badge>
+                    </ListGroup.Item>
+                    <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
+                        <div className="ms-2 me-auto">
+                            Sets
+                        </div>
+                        <Badge bg="success" pill>
+                          {routine.current_sets}
+                        </Badge>
+                        &nbsp;
+                        <Badge bg="primary" pill>
+                          {routine.initial_sets}
+                        </Badge>
+                    </ListGroup.Item>
                 </ListGroup>
                 <Card.Text className="d-flex justify-content-center">
                         <ActionButton variant={"secondary"} title={"Edit Exercise"} onClick={handleButtonClick} />
--- client/src/components/RoutineFilters.jsx
+++ client/src/components/RoutineFilters.jsx
@@ -1,22 +1,45 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Dropdown from 'react-bootstrap/Dropdown';
 import ActionButton from './ActionButton.jsx';
 
-const RoutineFilters = ({handleSortSelected}) => {
+const RoutineFilters = ({handleSortSelected, handleFilterSelected}) => {    
     return (
         <Container fluid>
             <Row className="pb-2 justify-content-between">
                 <Col xs={4}>
                     My Exercise Routine:
                 </Col>
-                <Col xs={4}></Col>
+                
+                <Col xs={1}></Col>
+
+                <Col xs={1}> 
+                    <Dropdown>
+                        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                            Filter By Day
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={()=>handleFilterSelected("all")}>All</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>handleFilterSelected(1)}>Monday</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>handleFilterSelected(2)}>Tuesday</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>handleFilterSelected(3)}>Wednesday</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>handleFilterSelected(4)}>Thursday</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>handleFilterSelected(5)}>Friday</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>handleFilterSelected(6)}>Saturday</Dropdown.Item>
+                            <Dropdown.Item onClick={()=>handleFilterSelected(7)}>Sunday</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Col>
+                
+                <Col xs={2}></Col>
+
                 <Col xs={1}>Sort By:</Col>
                 <Col xs={1}>
                     <ActionButton variant="secondary" title="Priority" onClick={() => handleSortSelected('priority')} />
                 </Col>
                 <Col xs={2}>
-                    <ActionButton variant="secondary" title="Exercise ID" onClick={() => handleSortSelected('exercise_id')} />
+                    <ActionButton variant="secondary" title="Exercise Name" onClick={() => handleSortSelected('name','exercise')} />
                 </Col>
             </Row>
         </Container>
